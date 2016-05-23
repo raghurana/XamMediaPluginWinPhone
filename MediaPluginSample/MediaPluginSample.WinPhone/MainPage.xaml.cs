ee2a0849e626afa5ec4bbd53368eb3e32d932296
Raghu Rana <raghurana@gmail.com>
2016-05-23 21:54:40 +1000

    Media picker added.

--- MediaPluginSample/MediaPluginSample.WinPhone/MainPage.xaml.cs
+++ MediaPluginSample/MediaPluginSample.WinPhone/MainPage.xaml.cs
@@ -1,21 +1,76 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Windows.Phone.UI.Input;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace MediaPluginSample.WinPhone
 {
     public sealed partial class MainPage : Page
     {
+        private bool isMediaActive;
+
         public MainPage()
         {
             InitializeComponent();
             Button.Click += OnCameraButtonClick;
+            HardwareButtons.BackPressed += HardwareButtonsOnBackPressed;
         }
 
-        private void OnCameraButtonClick(object sender, RoutedEventArgs routedEventArgs)
+        private async void OnCameraButtonClick(object sender, RoutedEventArgs routedEventArgs)
         {
-            
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await ShowMessage(":( No camera available.");
+                    return;
+                }
+
+                isMediaActive = true;
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                if (file == null)
+                {
+                    await ShowMessage(":( Failed to capture photo.");
+                    return;
+                }
+
+                await ShowMessage($"File saved at : {file.Path}");
+            }
+
+            catch (Exception ex)
+            {
+                await ShowMessage(ex.Message);
+            }
+
+            finally
+            {
+                isMediaActive = false;
+            }
+        }
+
+        private void HardwareButtonsOnBackPressed(object sender, BackPressedEventArgs backPressedEventArgs)
+        {
+            backPressedEventArgs.Handled = isMediaActive;
+            isMediaActive = false;
+        }
+
+        private static async Task ShowMessage(string message)
+        {
+            var msgbox = new MessageDialog(message);
+            await msgbox.ShowAsync();
         }
     }
 }
